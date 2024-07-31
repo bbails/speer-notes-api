fc5586fa45fe90cd850c4c94cbce2384e2d430b6
Ben Bailey <benbailey902@gmail.com>
2024-07-31 07:54:07 -0400

    fix: modified bcrypt import

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
 import { Schema, model } from "mongoose"; 
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 const { compare, hash } = bcrypt;
 import jwt from "jsonwebtoken";
 const { sign } = jwt;
